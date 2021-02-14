458c8ccad6eb9cfe058619789f3290226050897c
dkchanxc <alexandergreenwood7@gmail.com>
2021-02-14 12:44:36 -0500

    Added the first revisions of my neff solver and electrical scripts for a PN phase modulator

---
+++ electrical_characteristics.py
@@ -0,0 +1,169 @@
+#Author: Alexander Greenwood
+#The following scripts have been used for my BASc thesis on a "Segmented Microring Modulator"
+
+
+#Based on the following papers:
+#Analytical Modeling of Silicon Microring and Microdisk Modulators With Electrical and Optical Dynamics
+#By Dubé-Demers et al.
+#Ultrafast pulse-amplitude modulation with a femtojoule silicon photonic modulator
+#By Dubé-Demers et al.
+#Analytical Model and Fringing-Field Parasitics of Carrier-Depletion Silicon-on-Insulator Optical Modulation Diodes
+#By Jayatilleka et al.
+
+
+#This is a work in progress :)
+
+import matplotlib.pyplot as mpl
+import math
+import numpy as np
+import scipy as scipy
+from scipy import special
+#First define some physical constants
+n_i = 1.1e10 #intrinsic carrier concentration [cm^-3]
+k_b = 1.38*10**(-23) #Boltzmann's constant [JK^-1]
+epsilon_0 = 8.85*10**(-14) #permittivity of free space [F/cm] 
+epsilon_s = 11.7 # relative permittivity of silicon
+epsilon_sio =3.9
+q = 1.602*10**(-19) # charge of an electron [C]
+temp = 300 # [K]
+pi = 3.14
+mu_n = 364
+mu_p = 205
+
+#d_n = k*T / q * mu_n
+#d_p = k*T / q * mu_p
+
+#l_p = np.sqrt(d_p*tau_p) # diffusion length for holes
+#l_n = np.sqrt(d_n*tau_n) # diffusion length for electrons
+
+
+NPOINTS = 100
+#############################################################
+
+def get_carrier_concentrations(N_A,N_D,V,junction_width):
+    p_0 = n_i**2 / N_A
+    n_0 = n_i**2 / N_D
+
+    x = np.linspace(0,junction_width,NPOINTS)
+
+    n_0_profile = np.full((1,int(NPOINTS/2)),n_0 *-1)
+    p_0_profile = np.full((1,int(NPOINTS/2)),p_0)
+    
+    #carrier_profile = np.append(n_0_profile,p_0_profile)
+    #Use einstein's relation for obtaining diffusion const
+
+    
+    
+
+    mpl.plot(x,carrier_profile)
+    mpl.show()
+def get_depletion_width(N_A,N_D,V_bi,V):
+    inner = ((2*epsilon_0 *epsilon_s)/q)*((N_A+N_D)/(N_A*N_D))*(V_bi-V-(2*k_b*temp)/q)
+    w_d = np.sqrt(inner)
+
+    w_dp = w_d / (1 + (N_A/N_D))
+    w_dn = w_d / (1 + (N_D/N_A))
+    
+    inner = ((2*epsilon_0 *epsilon_sio)/q)*((N_A+N_D)/(N_A*N_D))*(V_bi-V-(2*k_b*temp)/q)
+    w_d_tb = np.sqrt(inner)
+
+    w_dp_tb = w_d_tb / (1 + (N_A/N_D))
+    w_dn_tb = w_d_tb / (1 + (N_D/N_A))
+
+    w_d *=0.01
+    w_dp *=0.01
+    w_dn *=0.01
+    w_dp_tb *=0.01
+    w_dn_tb *= 0.01
+
+    return [w_d,w_dp,w_dn,w_dp_tb,w_dn_tb]
+
+def get_vbi(N_A,N_D):
+    p_0 = n_i**2 / N_A
+    n_0 = n_i**2 / N_D
+    V_bi = k_b*temp / (2*q) * math.log(N_A*N_D / (p_0*n_0))
+    return V_bi
+
+def get_c_parallel(h_rib,w_d):
+    #usually h_rib is just 200 nm
+    c_parallel = (epsilon_0*epsilon_s*h_rib)/w_d
+    print(c_parallel)
+    return c_parallel
+
+def get_c_fringe(h_rib,w_d):
+    c_fringe = epsilon_0*(2*epsilon_sio / (2*np.pi))*math.log(2*np.pi * h_rib / w_d)
+    print(c_fringe)
+    return c_fringe
+def get_c_vertical(w_d,t_p,t_n):
+    k = np.sqrt(w_d*(w_d + t_p)*(w_d + t_n + t_p)/ (w_d + t_n))
+    m = k**2
+    k_prime = np.sqrt(1 - k**2)
+    m_prime = k_prime**2
+
+    c_top = epsilon_0 * epsilon_sio * scipy.special.ellipk(m_prime)/ scipy.special.ellipk(m)
+
+    return c_top
+
+def get_t(w_dp,w_dn,w_dp_tb,w_dn_tb,w_rib):
+    #start with expression for x_p, x_n
+    #Given N_a |x_p| = N_d |x_n
+    v = np.linspace(-np.pi/2,0,NPOINTS)
+    x_p = (2*w_dp_tb / np.pi) + w_dp - (4*w_dp_tb / np.pi * np.sin(v/2)**2)
+    x_n = (4*w_dn_tb / np.pi * np.sin(v/2)**2) - ((2*w_dn_tb / np.pi) + w_dn)
+    y = (2*w_dn_tb / np.pi)*(np.log(np.tan(np.pi/4 + v/2))-np.sin(v))
+    
+    mpl.plot(x_p,y,x_n,y)
+    t_n = 2*w_dn_tb / np.pi
+    t_p = 2*w_dp_tb / np.pi
+    mpl.show()
+    return [t_p,t_n]
+
+def get_r_np(w_pn,w_rib,N_A,N_D,h_slab,L,w_dn,w_dp,x_offset,h_rib):
+    w_pn *=100
+    w_rib *=100
+    L *=100
+    x_offset *=100
+    w_dp *=100
+    w_dn *=100
+    h_rib *=100
+
+    r_p = (w_pn - w_rib)/(2*q*N_A*mu_p*h_slab*L) + (w_rib/2 + x_offset - w_dp)/(q*N_A*mu_p*h_rib*L)
+    r_n = (w_pn - w_rib)/(2*q*N_D*mu_n*h_slab*L) + (w_rib/2 - x_offset - w_dn)/(q*N_D*mu_n*h_rib*L)
+    
+    print("L = " + str(L))
+    print("Rp = " + str(r_p))
+    print("Rn= " + str(r_n))
+    return [r_p, r_n]
+
+############################################################
+
+L = 0.75 * 2 * np.pi * 5e-6 # assuming radius is 5 um.
+x_offset = 120e-9
+w_pn = 1.2e-6
+h_slab = 90e-9  
+w_rib = 5e-7
+h_rib = 200e-9
+#Values below taken from Analytical Model and Fringing-Field Parasitics of Carrier-Depletion Silicon-on-Insulator Optical Modulation Diodes
+N_a = 5e18
+N_d = 5e18
+#get_carrier_concentrations(N_a,N_d,-1,1.5e-6)
+V_bi = get_vbi(N_a,N_d)
+w_d = get_depletion_width(N_a,N_d,V_bi,-1)
+
+w_dp = w_d[1]
+w_dn = w_d[2]
+w_dp_tb = w_d[3]
+w_dn_tb = w_d[4]
+w_d = w_d[0]
+
+t= get_t(w_dp,w_dn,w_dp_tb,w_dn_tb,w_rib)
+t_p = t[0]
+t_n = t[1]
+c_vertical = get_c_vertical(w_d,t_p,t_n)
+c_p = get_c_parallel(h_rib,w_d)
+c_f = get_c_fringe(h_rib,w_d)
+r_np = get_r_np(w_pn,w_rib,N_a,N_d,h_slab,L,w_dn,w_dp,x_offset,h_rib)
+#bw = (2*np.pi*(r_np[0]+r_np[1])*(c_vertical+c_f+c_p)*L)**(-1)
+A = (q/w_pn)*(mu_p*mu_n*N_a*N_d/(mu_p*N_a + mu_n*N_d))
+bw = A/(2*np.pi)*(h_slab/h_rib)*(epsilon_0*epsilon_s / w_d + (c_f + c_vertical)/h_rib)**(-1)
+print(str(bw/(1e9)) + " GHz")
---
+++ neff_solver.py
@@ -0,0 +1,49 @@
+#Author: Alexander Greenwood
+#The following scripts have been used for my BASc thesis on a "Segmented Microring Modulator"
+
+
+#Based on the following sources:
+#Silicon Photonics Design
+#By Professor Lukas Chrostowski, Professor Michael Hochberg
+#Photonics
+#By Prof. Amnon Yariv, Prof. Pochi Yeh
+
+
+#This is a work in progress :)
+
+import math
+import numpy as np
+#First define some physical constants
+n_i = 1.1*10**10 #intrinsic carrier concentration [cm^-3]
+k_b = 1.38*10**(-23) #Boltzmann's constant [JK^-1]
+epsilon_o = 8.85*10**(-14) #permittivity of free space [F/m] 
+q = 1.602*10**(-19) # charge of an electron [C]
+temp = 300 # [K]
+pi = 3.14
+#############################################################
+def get_neff(d, wavelength, n1, n2):
+#Obtain effective index profile for our waveguide
+    k0 = 2*pi / wavelength
+    k = n2*k0
+    beta_0 = np.linspace(n1*k0,n2*k0, num=1000)# Our propagation constant is somewhere between these two spatial freq's 
+    h = np.sqrt(k**2 - beta_0**2) 
+    q = np.sqrt(beta_0**2 - (k0*n1)**2)
+    te_rel = np.tan(h*d)*(h**2 - q**2) - 2*h*q # The zeros of this relation correspond to our modes!
+    zeros = list() 
+    intervals = (te_rel>=0).astype(int) - (te_rel<0).astype(int)
+    ############################################
+    #find indexes of zeros!
+    for i in range(1,te_rel.size):
+        if(intervals[i]-intervals[i-1]>0):
+            zeros.append(i)
+
+    neff = beta_0[zeros] / k0
+    print(neff)
+
+############################################################# 
+def get_coupling_coeff(coupling_length,spacing,d, wavelength, n1, n2): 
+    printf("This is a placeholder")
+
+
+
+get_neff(220e-9,1.55e-6,1.44,3.47)
